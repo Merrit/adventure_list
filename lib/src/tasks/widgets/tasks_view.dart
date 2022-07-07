c80e386a3a48738edb436b2de553506c562e5dcb
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-07-07 14:08:42 -0400

    Hide completed items panel

--- lib/src/tasks/widgets/tasks_view.dart
+++ lib/src/tasks/widgets/tasks_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:helpers/helpers.dart';
 
-import '../../core/core.dart';
 import '../tasks.dart';
 
 class TasksView extends StatelessWidget {
@@ -32,14 +31,14 @@ class TasksView extends StatelessWidget {
                       .toList(),
                 ),
               ),
-              if (state.activeList!.items.any((element) => element.completed))
-                ExpansionTile(
-                  title: const Text('Completed'),
-                  children: state.activeList!.items
-                      .where((element) => element.completed)
-                      .map((e) => TaskTile(task: e))
-                      .toList(),
-                ),
+              // if (state.activeList!.items.any((element) => element.completed))
+              //   ExpansionTile(
+              //     title: const Text('Completed'),
+              //     children: state.activeList!.items
+              //         .where((element) => element.completed)
+              //         .map((e) => TaskTile(task: e))
+              //         .toList(),
+              //   ),
             ],
           ),
         );
