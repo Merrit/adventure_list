7f883326c3c58def4654e30c2166abc25dac979f
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-07-07 15:01:33 -0400

    Use flex_color_scheme for themes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,9 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'authentication/authentication.dart';
 import 'authentication/login_page.dart';
@@ -35,8 +37,32 @@ class App extends StatelessWidget {
           ],
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
+          theme: FlexThemeData.light(
+            scheme: FlexScheme.blue,
+            surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+            blendLevel: 20,
+            appBarOpacity: 0.95,
+            subThemesData: const FlexSubThemesData(
+              blendOnLevel: 20,
+              blendOnColors: false,
+            ),
+            visualDensity: FlexColorScheme.comfortablePlatformDensity,
+            useMaterial3: true,
+            fontFamily: GoogleFonts.notoSans().fontFamily,
+          ),
+          darkTheme: FlexThemeData.dark(
+            scheme: FlexScheme.blue,
+            surfaceMode: FlexSurfaceMode.highSurfaceLowScaffold,
+            blendLevel: 40,
+            appBarStyle: FlexAppBarStyle.background,
+            appBarOpacity: 0.90,
+            subThemesData: const FlexSubThemesData(
+              blendOnLevel: 30,
+            ),
+            visualDensity: FlexColorScheme.comfortablePlatformDensity,
+            useMaterial3: true,
+            fontFamily: GoogleFonts.notoSans().fontFamily,
+          ),
           themeMode: ThemeMode.system,
           onGenerateRoute: (RouteSettings routeSettings) {
             return MaterialPageRoute<void>(
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   url_launcher_linux
-  window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
