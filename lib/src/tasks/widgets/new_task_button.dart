a8d940deee80f9069402daba3b0375f2d38aa1c5
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-07-13 16:19:05 -0400

    Refactor NewTaskButton

--- lib/src/tasks/widgets/new_task_button.dart
+++ lib/src/tasks/widgets/new_task_button.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:helpers/helpers.dart';
 
 import '../tasks.dart';
 
 /// Button for creating new tasks.
-///
-/// Initially a less emphasized `ListTile`, when clicked the title becomes a
-/// TextField so the user can enter a name for the new task; when the user is
-/// finished entering the new name it transforms back into the base widget
-/// version of itself to be used again.
 class NewTaskButton extends StatefulWidget {
   const NewTaskButton({
     Key? key,
@@ -19,7 +13,8 @@ class NewTaskButton extends StatefulWidget {
 }
 
 class _NewTaskButtonState extends State<NewTaskButton> {
-  final TextEditingController controller = TextEditingController();
+  final TextEditingController controller = TextEditingController()
+    ..text = 'New Task';
   final FocusNode focusNode = FocusNode(
     debugLabel: 'NewTaskButton FocusNode',
   );
@@ -31,155 +26,52 @@ class _NewTaskButtonState extends State<NewTaskButton> {
     super.dispose();
   }
 
-  bool _showTextField = false;
+  double opacity = 0.5;
 
   @override
   Widget build(BuildContext context) {
     focusNode.addListener(() {
-      // When focus is lost, switch back to showing the Text widget.
-      if (!focusNode.hasFocus) setState(() => _showTextField = false);
+      if (focusNode.hasFocus) {
+        setState(() => opacity = 1.0);
+        controller.text = '';
+      } else {
+        setState(() => opacity = 0.5);
+        controller.text = 'New Task';
+      }
     });
-
-    controller.text = 'Name';
 
     void _createTask() {
       tasksCubit.createTask(Task(title: controller.text));
       FocusManager.instance.primaryFocus?.unfocus();
     }
 
-    Widget child;
-    if (_showTextField) {
-      child = TextField(
-        focusNode: focusNode,
-        controller: controller..selectAll(),
-        autofocus: true,
-        textAlignVertical: TextAlignVertical.center,
-        keyboardType: TextInputType.text,
-        textCapitalization: TextCapitalization.words,
-        onSubmitted: (_) => _createTask(),
-      );
-    } else {
-      child = const Text('New Task');
-    }
-
     return Opacity(
-      opacity: _showTextField ? 1 : 0.5,
+      opacity: opacity,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
-        child: ListTile(
-          leading: const Icon(Icons.add),
-          title: child,
-          onTap: () {
-            focusNode.requestFocus();
-            setState(() => _showTextField = true);
-          },
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            inputDecorationTheme: const InputDecorationTheme(
+              border: InputBorder.none,
+            ),
+          ),
+          child: ListTile(
+            hoverColor: Colors.transparent,
+            focusColor: Colors.transparent,
+            leading: const Icon(Icons.add),
+            // title: child,
+            title: TextField(
+              controller: controller,
+              focusNode: focusNode,
+              textCapitalization: TextCapitalization.words,
+              onSubmitted: (_) => _createTask(),
+            ),
+            onTap: () {
+              focusNode.requestFocus();
+            },
+          ),
         ),
       ),
     );
   }
 }
-
-// TODO: Can we genericize this widget pattern? Widget -> TextField -> Widget
-//
-// Doesn't work in this instance, because we only want the ListTile's title to
-// be replaced by TextField, but have the entire ListTile be clickable to enter
-// TextField mode. Need to experiment with this more.
-//
-/// Widget which when clicked becomes a `TextField`, and when submitted calls
-/// `onSubmitted` with the entered text and becomes the originally provided
-/// widget again.
-// class TextFieldSwapWidget extends StatefulWidget {
-//   final Widget defaultChild;
-//   final FocusNode? focusNode;
-//   final void Function(String value) onSubmitted;
-//   final TextAlign? textAlign;
-//   final String? textFieldInitialText;
-
-//   const TextFieldSwapWidget({
-//     Key? key,
-//     required this.defaultChild,
-//     this.focusNode,
-//     required this.onSubmitted,
-//     this.textAlign,
-//     this.textFieldInitialText,
-//   }) : super(key: key);
-
-//   @override
-//   State<TextFieldSwapWidget> createState() => _TextFieldSwapWidgetState();
-// }
-
-// class _TextFieldSwapWidgetState extends State<TextFieldSwapWidget> {
-//   final TextEditingController controller = TextEditingController();
-//   late final FocusNode focusNode;
-
-//   @override
-//   void initState() {
-//     focusNode = widget.focusNode ??
-//         FocusNode(
-//           debugLabel: 'TextFieldSwapWidget FocusNode',
-//         )
-//       ..addListener(() {
-//         // When focus is lost, switch back to showing the Text widget.
-//         print('focus listener triggered');
-//         if (focusNode.hasFocus) {
-//           setState(() => _showTextField = true);
-//         } else {
-//           setState(() => _showTextField = false);
-//         }
-//       });
-//     super.initState();
-//   }
-
-//   @override
-//   void dispose() {
-//     controller.dispose();
-//     focusNode.dispose();
-//     super.dispose();
-//   }
-
-//   bool _showTextField = false;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // focusNode.addListener(() {
-//     //   // When focus is lost, switch back to showing the Text widget.
-//     //   print('focus listener triggered');
-//     //   if (focusNode.hasFocus) {
-//     //     setState(() => _showTextField = true);
-//     //   } else {
-//     //     setState(() => _showTextField = false);
-//     //   }
-//     // });
-
-//     controller.text = widget.textFieldInitialText ?? '';
-
-//     void _submit() {
-//       widget.onSubmitted(controller.text);
-//       FocusManager.instance.primaryFocus?.unfocus();
-//     }
-
-//     Widget child;
-//     if (_showTextField) {
-//       child = TextField(
-//         focusNode: focusNode,
-//         controller: controller..selectAll(),
-//         autofocus: true,
-//         textAlign: widget.textAlign ?? TextAlign.start,
-//         textAlignVertical: TextAlignVertical.center,
-//         keyboardType: TextInputType.text,
-//         textCapitalization: TextCapitalization.words,
-//         onSubmitted: (_) => _submit(),
-//       );
-//     } else {
-//       child = widget.defaultChild;
-//     }
-
-//     return InkWell(
-//       onTap: () {
-//         focusNode.requestFocus();
-//         setState(() => _showTextField = true);
-//       },
-//       child: child,
-//     );
-//   }
-// }
