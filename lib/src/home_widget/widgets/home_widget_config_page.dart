61a74d735d17f6497630130552708e3da2d610b4
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-07-18 11:53:29 -0400

    Widget config: back button goes to main screen

--- lib/src/home_widget/widgets/home_widget_config_page.dart
+++ lib/src/home_widget/widgets/home_widget_config_page.dart
@@ -13,53 +13,61 @@ class HomeWidgetConfigPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Configure Home Widget')),
-      body: BlocBuilder<SettingsCubit, SettingsState>(
-        builder: (context, settingsState) {
-          return BlocBuilder<TasksCubit, TasksState>(
-            builder: (context, tasksState) {
-              return ListView(
-                children: [
-                  ...tasksState.taskLists
-                      .map((e) => RadioListTile(
-                            value: e.id,
-                            groupValue: settingsState.homeWidgetSelectedListId,
-                            onChanged: (String? value) async {
-                              if (value == null) return;
+    return WillPopScope(
+      onWillPop: () {
+        // Ensure the back button goes to the main screen.
+        Navigator.pushReplacementNamed(context, TasksPage.routeName);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Configure Home Widget')),
+        body: BlocBuilder<SettingsCubit, SettingsState>(
+          builder: (context, settingsState) {
+            return BlocBuilder<TasksCubit, TasksState>(
+              builder: (context, tasksState) {
+                return ListView(
+                  children: [
+                    ...tasksState.taskLists
+                        .map((e) => RadioListTile(
+                              value: e.id,
+                              groupValue:
+                                  settingsState.homeWidgetSelectedListId,
+                              onChanged: (String? value) async {
+                                if (value == null) return;
 
-                              settingsCubit.updateHomeWidgetSelectedListId(
-                                value,
-                              );
+                                settingsCubit.updateHomeWidgetSelectedListId(
+                                  value,
+                                );
 
-                              final selectedList = tasksState //
-                                  .taskLists
-                                  .singleWhere(
-                                (taskList) => taskList.id == value,
-                              );
+                                final selectedList = tasksState //
+                                    .taskLists
+                                    .singleWhere(
+                                  (taskList) => taskList.id == value,
+                                );
 
-                              await updateHomeWidget(
-                                'selectedList',
-                                selectedList.toJson(),
-                              );
-                            },
-                            title: Text(e.title),
-                          ))
-                      .toList(),
-                ],
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () async {
-          final navigator = Navigator.of(context);
-          await MoveToBackground.moveTaskToBack();
-          navigator.pushReplacementNamed(TasksPage.routeName);
-        },
-        // onPressed: () => SystemNavigator.pop(),
-        label: const Text('Done'),
+                                await updateHomeWidget(
+                                  'selectedList',
+                                  selectedList.toJson(),
+                                );
+                              },
+                              title: Text(e.title),
+                            ))
+                        .toList(),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () async {
+            final navigator = Navigator.of(context);
+            await MoveToBackground.moveTaskToBack();
+            navigator.pushReplacementNamed(TasksPage.routeName);
+          },
+          // onPressed: () => SystemNavigator.pop(),
+          label: const Text('Done'),
+        ),
       ),
     );
   }
