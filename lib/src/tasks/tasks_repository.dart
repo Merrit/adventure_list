802c4cd7b450bfdaffed4a94fd9a764eff9a6386
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-08-04 15:15:21 -0400

    TasksRepository: add doc comments and TODOs

--- lib/src/tasks/tasks_repository.dart
+++ lib/src/tasks/tasks_repository.dart
@@ -3,7 +3,14 @@ import 'package:googleapis_auth/googleapis_auth.dart';
 import '../authentication/authentication.dart';
 import 'tasks.dart';
 
+/// Abstract interface for task repositories.
+///
+/// Currently supports [GoogleCalendar].
+///
+/// Support for a local-only repository is being considered.
 abstract class TasksRepository {
+  // TODO: Genericize this initializer for non-calendar.
+  // Maybe just have the initializer in the concrete classes.
   static Future<TasksRepository> initialize({
     required ClientId clientId,
     required AccessCredentials credentials,
@@ -14,21 +21,27 @@ abstract class TasksRepository {
     );
   }
 
+  /// Fetch all of the user's [TaskList]s.
   Future<List<TaskList>> getAll();
 
+  /// Create a new [TaskList].
   Future<TaskList> createList({required String title});
 
+  /// Delete the [TaskList] with [id].
   Future<void> deleteList({required String id});
 
+  /// Update the provided [TaskList] in the repository.
   Future<void> updateList({required TaskList list});
 
+  /// Create a new [Task].
   Future<Task> createTask({
-    required String calendarId,
+    required String calendarId, // TODO: Genericize this for non-calendar.
     required Task newTask,
   });
 
+  /// Update the provided [Task] in the repository.
   Future<Task> updateTask({
-    required String calendarId,
+    required String calendarId, // TODO: Genericize this for non-calendar.
     required Task updatedTask,
   });
 }
