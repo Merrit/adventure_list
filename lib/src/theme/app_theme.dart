258240465ff8d64dde6a9e68021e59a9432773b8
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-02 14:28:52 -0500

    feat: migrate theme to Material 3

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -1,49 +1,21 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final ThemeData _flexDarkTheme = FlexThemeData.dark(
-  scheme: FlexScheme.blue,
-  surfaceMode: FlexSurfaceMode.highSurfaceLowScaffold,
-  blendLevel: 40,
-  appBarStyle: FlexAppBarStyle.background,
-  appBarOpacity: 0.90,
-  subThemesData: const FlexSubThemesData(
-    blendOnLevel: 30,
-    dialogRadius: 14.0,
-    timePickerDialogRadius: 14.0,
-  ),
-  visualDensity: FlexColorScheme.comfortablePlatformDensity,
-  // useMaterial3: true,
+/// The app's branded color.
+const appColor = Color.fromRGBO(0, 179, 255, 1);
+
+/// Dark app theme.
+final ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.dark,
+  colorSchemeSeed: appColor,
   fontFamily: GoogleFonts.notoSans().fontFamily,
 );
 
-final ThemeData _flexLightTheme = FlexThemeData.light(
-  scheme: FlexScheme.blue,
-  surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-  blendLevel: 20,
-  appBarOpacity: 0.95,
-  subThemesData: const FlexSubThemesData(
-    blendOnLevel: 20,
-    blendOnColors: false,
-    dialogRadius: 14.0,
-    timePickerDialogRadius: 14.0,
-  ),
-  visualDensity: FlexColorScheme.comfortablePlatformDensity,
-  // useMaterial3: true,
+/// Light app theme.
+final ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.light,
+  colorSchemeSeed: appColor,
   fontFamily: GoogleFonts.notoSans().fontFamily,
-);
-
-final ThemeData darkTheme = _flexDarkTheme.copyWith(
-  // Impossible to read button by default.
-  snackBarTheme: _flexDarkTheme.snackBarTheme.copyWith(
-    actionTextColor: _flexDarkTheme.colorScheme.onPrimary,
-  ),
-);
-
-final ThemeData lightTheme = _flexLightTheme.copyWith(
-  // Impossible to read button by default.
-  snackBarTheme: _flexLightTheme.snackBarTheme.copyWith(
-    actionTextColor: _flexLightTheme.colorScheme.onPrimary,
-  ),
 );
