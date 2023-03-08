0663da76bdcced220e4e4fc1f2e503a6c3df3a8f
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-03-08 10:12:45 -0500

    refactor: SettingsState uses freezed

--- lib/src/settings/cubit/settings_state.dart
+++ lib/src/settings/cubit/settings_state.dart
@@ -1,39 +1,12 @@
 part of 'settings_cubit.dart';
 
-class SettingsState extends Equatable {
-  final bool closeToTray;
+@freezed
+class SettingsState with _$SettingsState {
+  const factory SettingsState({
+    required bool closeToTray,
 
-  /// The id of the list to show in the Android home widget / AppWidget.
-  final String homeWidgetSelectedListId;
-
-  final ThemeData theme;
-
-  const SettingsState({
-    required this.closeToTray,
-    required this.homeWidgetSelectedListId,
-    required this.theme,
-  });
-
-  @override
-  List<Object> get props {
-    return [
-      closeToTray,
-      homeWidgetSelectedListId,
-      theme,
-    ];
-  }
-
-  SettingsState copyWith({
-    bool? closeToTray,
-    String? homeWidgetSelectedListId,
-    bool? logToFile,
-    ThemeData? theme,
-  }) {
-    return SettingsState(
-      closeToTray: closeToTray ?? this.closeToTray,
-      homeWidgetSelectedListId:
-          homeWidgetSelectedListId ?? this.homeWidgetSelectedListId,
-      theme: theme ?? this.theme,
-    );
-  }
+    /// The id of the list to show in the Android home widget / AppWidget.
+    required String homeWidgetSelectedListId,
+    required ThemeData theme,
+  }) = _SettingsState;
 }
