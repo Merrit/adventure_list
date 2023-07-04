6b67da2d5fa365673f54d8f69c3ba3b116b7e86a
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-07-04 15:14:21 -0400

    fix: improve reliability of release notes dialog

--- lib/src/tasks/widgets/tasks_page.dart
+++ lib/src/tasks/widgets/tasks_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:helpers/helpers.dart';
 
+import '../../app/app.dart';
+import '../../core/constants.dart';
 import '../../core/helpers/helpers.dart';
 import '../../notifications/notifications.dart';
 import '../../settings/settings.dart';
@@ -27,102 +30,134 @@ class _TasksPageState extends State<TasksPage> {
     final mediaQuery = MediaQuery.of(context);
 
     return SafeArea(
-      child: BlocConsumer<TasksCubit, TasksState>(
-        listener: (context, state) {
-          // If there is an active task and we are on a small screen, we want to
-          // navigate to the task details page.
-          if (state.activeTask != null && mediaQuery.isSmallScreen) {
-            Navigator.pushNamed(context, TaskDetails.routeName);
-          }
-        },
+      child: BlocBuilder<AppCubit, AppState>(
         builder: (context, state) {
-          if (state.loading) {
-            return const Center(child: CircularProgressIndicator());
-          }
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            if (state.releaseNotes != null) {
+              _showReleaseNotesDialog(context, state.releaseNotes!);
+            }
+          });
 
-          final bool pinned;
-          if (defaultTargetPlatform.isDesktop) {
-            pinned = context.select<WindowCubit, bool>(
-              (cubit) => cubit.state.pinned,
-            );
-          } else {
-            pinned = false;
-          }
+          return BlocConsumer<TasksCubit, TasksState>(
+            listener: (context, state) {
+              // If there is an active task and we are on a small screen, we want to
+              // navigate to the task details page.
+              if (state.activeTask != null && mediaQuery.isSmallScreen) {
+                Navigator.pushNamed(context, TaskDetails.routeName);
+              }
+            },
+            builder: (context, state) {
+              if (state.loading) {
+                return const Center(child: CircularProgressIndicator());
+              }
 
-          final bool transparentBackgroundEnabled;
-          if (defaultTargetPlatform.isDesktop) {
-            transparentBackgroundEnabled = context.select<SettingsCubit, bool>(
-              (cubit) =>
-                  cubit.state.desktopWidgetSettings.transparentBackground,
-            );
-          } else {
-            transparentBackgroundEnabled = false;
-          }
+              final bool pinned;
+              if (defaultTargetPlatform.isDesktop) {
+                pinned = context.select<WindowCubit, bool>(
+                  (cubit) => cubit.state.pinned,
+                );
+              } else {
+                pinned = false;
+              }
 
-          final Color backgroundColor = (pinned && transparentBackgroundEnabled)
-              ? Colors.transparent
-              : Theme.of(context).scaffoldBackgroundColor;
+              final bool transparentBackgroundEnabled;
+              if (defaultTargetPlatform.isDesktop) {
+                transparentBackgroundEnabled =
+                    context.select<SettingsCubit, bool>(
+                  (cubit) =>
+                      cubit.state.desktopWidgetSettings.transparentBackground,
+                );
+              } else {
+                transparentBackgroundEnabled = false;
+              }
 
-          final Widget bodyContainer = Row(
-            children: [
-              if (!mediaQuery.isSmallScreen) const VerticalDivider(),
-              const Flexible(
-                flex: 1,
-                child: TasksView(),
-              ),
-              if (!mediaQuery.isSmallScreen) const VerticalDivider(),
-            ],
+              final Color backgroundColor =
+                  (pinned && transparentBackgroundEnabled)
+                      ? Colors.transparent
+                      : Theme.of(context).scaffoldBackgroundColor;
+
+              final Widget bodyContainer = Row(
+                children: [
+                  if (!mediaQuery.isSmallScreen) const VerticalDivider(),
+                  const Flexible(
+                    flex: 1,
+                    child: TasksView(),
+                  ),
+                  if (!mediaQuery.isSmallScreen) const VerticalDivider(),
+                ],
+              );
+
+              final PreferredSizeWidget? appBar;
+              if (mediaQuery.isSmallScreen) {
+                appBar = const _TaskListAppBar();
+              } else if (kDebugMode) {
+                appBar = AppBar(
+                  title: const Text('Debug'),
+                  actions: const [_DebugMenuButton()],
+                  backgroundColor: backgroundColor,
+                );
+              } else {
+                appBar = null;
+              }
+
+              return Scaffold(
+                appBar: appBar,
+                drawer: (mediaQuery.isSmallScreen)
+                    ? const CustomNavigationRail(breakpoint: Breakpoints.small)
+                    : null,
+                backgroundColor: backgroundColor,
+                body: AdaptiveLayout(
+                  primaryNavigation: SlotLayout(
+                    config: <Breakpoint, SlotLayoutConfig>{
+                      Breakpoints.large: SlotLayout.from(
+                        key: const Key('Primary Navigation Large'),
+                        // inAnimation: AdaptiveScaffold.leftOutIn,
+                        builder: (_) => const CustomNavigationRail(
+                          breakpoint: Breakpoints.large,
+                        ),
+                      ),
+                    },
+                  ),
+                  body: SlotLayout(
+                    config: {
+                      Breakpoints.standard: SlotLayout.from(
+                        key: const Key('Body Standard'),
+                        builder: (_) => bodyContainer,
+                      ),
+                    },
+                  ),
+                  secondaryBody: SlotLayout(
+                    config: {
+                      Breakpoints.large: SlotLayout.from(
+                        key: const Key('Secondary Body Standard'),
+                        builder: (_) => const TaskDetails(),
+                      ),
+                    },
+                  ),
+                ),
+              );
+            },
           );
+        },
+      ),
+    );
+  }
 
-          final PreferredSizeWidget? appBar;
-          if (mediaQuery.isSmallScreen) {
-            appBar = const _TaskListAppBar();
-          } else if (kDebugMode) {
-            appBar = AppBar(
-              title: const Text('Debug'),
-              actions: const [_DebugMenuButton()],
-              backgroundColor: backgroundColor,
-            );
-          } else {
-            appBar = null;
-          }
+  Future<void> _showReleaseNotesDialog(
+    BuildContext context,
+    ReleaseNotes releaseNotes,
+  ) {
+    final appCubit = context.read<AppCubit>();
 
-          return Scaffold(
-            appBar: appBar,
-            drawer: (mediaQuery.isSmallScreen)
-                ? const CustomNavigationRail(breakpoint: Breakpoints.small)
-                : null,
-            backgroundColor: backgroundColor,
-            body: AdaptiveLayout(
-              primaryNavigation: SlotLayout(
-                config: <Breakpoint, SlotLayoutConfig>{
-                  Breakpoints.large: SlotLayout.from(
-                    key: const Key('Primary Navigation Large'),
-                    // inAnimation: AdaptiveScaffold.leftOutIn,
-                    builder: (_) => const CustomNavigationRail(
-                      breakpoint: Breakpoints.large,
-                    ),
-                  ),
-                },
-              ),
-              body: SlotLayout(
-                config: {
-                  Breakpoints.standard: SlotLayout.from(
-                    key: const Key('Body Standard'),
-                    builder: (_) => bodyContainer,
-                  ),
-                },
-              ),
-              secondaryBody: SlotLayout(
-                config: {
-                  Breakpoints.large: SlotLayout.from(
-                    key: const Key('Secondary Body Standard'),
-                    builder: (_) => const TaskDetails(),
-                  ),
-                },
-              ),
-            ),
-          );
+    return showDialog(
+      context: context,
+      builder: (context) => ReleaseNotesDialog(
+        releaseNotes: releaseNotes,
+        donateCallback: () => appCubit.launchURL(kDonateUrl),
+        launchURL: (url) => appCubit.launchURL(url),
+        onClose: () {
+          appCubit.dismissReleaseNotesDialog();
+          Navigator.pop(context);
         },
       ),
     );
