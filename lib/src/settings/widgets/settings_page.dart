74d41fd92cbe07bf565cbfa662b93e4405c166d9
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-08-04 15:15:32 -0400

    feat: add verbose logging toggle to settings

--- lib/src/settings/widgets/settings_page.dart
+++ lib/src/settings/widgets/settings_page.dart
@@ -13,6 +13,7 @@ import '../../authentication/sign_in_page.dart';
 import '../../core/core.dart';
 import '../../core/helpers/helpers.dart';
 import '../../home_widget/home_widget.dart';
+import '../../logs/logging_manager.dart';
 import '../../theme/theme.dart';
 import '../settings.dart';
 
@@ -46,14 +47,6 @@ class _SettingsView extends StatelessWidget {
           horizontalPadding = (constraints.maxWidth - 600) / 2;
         }
 
-        final versionSection = _SectionCard(
-          title: LocaleKeys.settings_version_title.tr(),
-          children: const [
-            _CurrentVersionTile(),
-            _UpdateTile(),
-          ],
-        );
-
         final appearanceSection = _SectionCard(
           title: LocaleKeys.settings_appearance_title.tr(),
           children: const [
@@ -84,17 +77,33 @@ class _SettingsView extends StatelessWidget {
           ],
         );
 
+        final troubleshootingSection = _SectionCard(
+          title: LocaleKeys.settings_troubleshooting_title.tr(),
+          children: const [
+            _VerboseLoggingTile(),
+          ],
+        );
+
+        final versionSection = _SectionCard(
+          title: LocaleKeys.settings_version_title.tr(),
+          children: const [
+            _CurrentVersionTile(),
+            _UpdateTile(),
+          ],
+        );
+
         return ListView(
           padding: EdgeInsets.symmetric(
             horizontal: horizontalPadding,
             vertical: 16,
           ),
           children: [
-            versionSection,
             appearanceSection,
             integrationSection,
             widgetSection,
             syncSection,
+            troubleshootingSection,
+            versionSection,
           ],
         );
       },
@@ -129,6 +138,200 @@ class _SectionCard extends StatelessWidget {
           ...children,
         ],
       ),
+    );
+  }
+}
+
+/// A tile that shows the current theme, and a switch to toggle between
+/// light and dark mode.
+class _ThemeTile extends StatelessWidget {
+  const _ThemeTile();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          value: state.theme == darkTheme,
+          title: Text(LocaleKeys.settings_appearance_darkMode.tr()),
+          secondary: const Icon(Icons.brightness_4),
+          onChanged: (bool value) {
+            context
+                .read<SettingsCubit>()
+                .updateThemeMode(value ? ThemeMode.dark : ThemeMode.light);
+          },
+        );
+      },
+    );
+  }
+}
+
+/// Shows whether the app is set to autostart, and a switch to toggle it.
+///
+/// Only available on Desktop platforms.
+class _AutostartTile extends StatelessWidget {
+  const _AutostartTile();
+
+  @override
+  Widget build(BuildContext context) {
+    if (!defaultTargetPlatform.isDesktop) return const SizedBox();
+
+    return BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          title: Row(
+            children: [
+              Text(LocaleKeys.settings_integration_autostart.tr()),
+              const SizedBox(width: 8),
+              Tooltip(
+                message: LocaleKeys.settings_integration_autostartDescription.tr(),
+                child: const Icon(Icons.info_outline),
+              ),
+            ],
+          ),
+          secondary: const Icon(Icons.autorenew),
+          value: state.autostart,
+          onChanged: (value) => context.read<SettingsCubit>().toggleAutostart(),
+        );
+      },
+    );
+  }
+}
+
+class _CloseToTrayTile extends StatelessWidget {
+  const _CloseToTrayTile();
+
+  @override
+  Widget build(BuildContext context) {
+    if (!Platform.isLinux && !Platform.isWindows) return const SizedBox();
+
+    return BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          title: Text(LocaleKeys.settings_integration_closeToTray.tr()),
+          secondary: const Icon(Icons.bedtime),
+          value: state.closeToTray,
+          onChanged: (value) => context.read<SettingsCubit>().updateCloseToTray(value),
+        );
+      },
+    );
+  }
+}
+
+class _CustomizeAndroidWidgetTile extends StatelessWidget {
+  const _CustomizeAndroidWidgetTile();
+
+  @override
+  Widget build(BuildContext context) {
+    if (!defaultTargetPlatform.isAndroid) return const SizedBox();
+
+    return ListTile(
+      title: Text(LocaleKeys.settings_settings.tr()),
+      trailing: const Icon(Icons.keyboard_arrow_right),
+      onTap: () => Navigator.pushNamed(context, HomeWidgetConfigPage.routeName),
+    );
+  }
+}
+
+class _CustomizeDesktopWidgetTile extends StatelessWidget {
+  const _CustomizeDesktopWidgetTile();
+
+  @override
+  Widget build(BuildContext context) {
+    if (!defaultTargetPlatform.isDesktop) return const SizedBox();
+
+    return BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          title: Text(LocaleKeys.settings_widget_transparentBackground.tr()),
+          secondary: const Icon(Icons.desktop_windows),
+          value: state.desktopWidgetSettings.transparentBackground,
+          onChanged: (value) => context.read<SettingsCubit>().updateDesktopWidgetSettings(
+                state.desktopWidgetSettings.copyWith(
+                  transparentBackground: value,
+                ),
+              ),
+        );
+      },
+    );
+  }
+}
+
+class _SignOutTile extends StatelessWidget {
+  const _SignOutTile();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AuthenticationCubit, AuthenticationState>(
+      builder: (context, authState) {
+        if (!authState.signedIn) {
+          return const SizedBox.shrink();
+        }
+
+        return ListTile(
+          title: Text(LocaleKeys.settings_sync_signOut.tr()),
+          leading: const Icon(Icons.logout),
+          onTap: () => _showSignOutDialog(context),
+        );
+      },
+    );
+  }
+
+  /// Shows a dialog to confirm signing out.
+  void _showSignOutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(LocaleKeys.settings_sync_signOut.tr()),
+          content: Text(LocaleKeys.settings_sync_confirmSignOut.tr()),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text(LocaleKeys.cancel.tr()),
+            ),
+            TextButton(
+              onPressed: () async {
+                final navigator = Navigator.of(context);
+
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) => const CircularProgressIndicator(),
+                );
+
+                await authCubit.signOut();
+                navigator.pushReplacementNamed(SignInPage.routeName);
+              },
+              child: Text(LocaleKeys.settings_sync_signOut.tr()),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _VerboseLoggingTile extends StatefulWidget {
+  const _VerboseLoggingTile();
+
+  @override
+  State<_VerboseLoggingTile> createState() => _VerboseLoggingTileState();
+}
+
+class _VerboseLoggingTileState extends State<_VerboseLoggingTile> {
+  bool isVerbose = LoggingManager.instance.verbose;
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      secondary: const Icon(Icons.bug_report_outlined),
+      title: const Text('Verbose logging'),
+      value: isVerbose,
+      onChanged: (value) async {
+        setState(() => isVerbose = value);
+        await LoggingManager.initialize(verbose: value);
+      },
     );
   }
 }
@@ -203,179 +406,6 @@ class _UpdateTile extends StatelessWidget {
             title: Text(LocaleKeys.settings_version_upToDate.tr()),
           );
         }
-      },
-    );
-  }
-}
-
-/// A tile that shows the current theme, and a switch to toggle between
-/// light and dark mode.
-class _ThemeTile extends StatelessWidget {
-  const _ThemeTile();
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<SettingsCubit, SettingsState>(
-      builder: (context, state) {
-        return SwitchListTile(
-          value: state.theme == darkTheme,
-          title: Text(LocaleKeys.settings_appearance_darkMode.tr()),
-          secondary: const Icon(Icons.brightness_4),
-          onChanged: (bool value) {
-            context
-                .read<SettingsCubit>()
-                .updateThemeMode(value ? ThemeMode.dark : ThemeMode.light);
-          },
-        );
-      },
-    );
-  }
-}
-
-/// Shows whether the app is set to autostart, and a switch to toggle it.
-///
-/// Only available on Desktop platforms.
-class _AutostartTile extends StatelessWidget {
-  const _AutostartTile();
-
-  @override
-  Widget build(BuildContext context) {
-    if (!defaultTargetPlatform.isDesktop) return const SizedBox();
-
-    return BlocBuilder<SettingsCubit, SettingsState>(
-      builder: (context, state) {
-        return SwitchListTile(
-          title: Row(
-            children: [
-              Text(LocaleKeys.settings_integration_autostart.tr()),
-              const SizedBox(width: 8),
-              Tooltip(
-                message:
-                    LocaleKeys.settings_integration_autostartDescription.tr(),
-                child: const Icon(Icons.info_outline),
-              ),
-            ],
-          ),
-          secondary: const Icon(Icons.autorenew),
-          value: state.autostart,
-          onChanged: (value) => context.read<SettingsCubit>().toggleAutostart(),
-        );
-      },
-    );
-  }
-}
-
-class _CloseToTrayTile extends StatelessWidget {
-  const _CloseToTrayTile();
-
-  @override
-  Widget build(BuildContext context) {
-    if (!Platform.isLinux && !Platform.isWindows) return const SizedBox();
-
-    return BlocBuilder<SettingsCubit, SettingsState>(
-      builder: (context, state) {
-        return SwitchListTile(
-          title: Text(LocaleKeys.settings_integration_closeToTray.tr()),
-          secondary: const Icon(Icons.bedtime),
-          value: state.closeToTray,
-          onChanged: (value) =>
-              context.read<SettingsCubit>().updateCloseToTray(value),
-        );
-      },
-    );
-  }
-}
-
-class _CustomizeAndroidWidgetTile extends StatelessWidget {
-  const _CustomizeAndroidWidgetTile();
-
-  @override
-  Widget build(BuildContext context) {
-    if (!defaultTargetPlatform.isAndroid) return const SizedBox();
-
-    return ListTile(
-      title: Text(LocaleKeys.settings_settings.tr()),
-      trailing: const Icon(Icons.keyboard_arrow_right),
-      onTap: () => Navigator.pushNamed(context, HomeWidgetConfigPage.routeName),
-    );
-  }
-}
-
-class _CustomizeDesktopWidgetTile extends StatelessWidget {
-  const _CustomizeDesktopWidgetTile();
-
-  @override
-  Widget build(BuildContext context) {
-    if (!defaultTargetPlatform.isDesktop) return const SizedBox();
-
-    return BlocBuilder<SettingsCubit, SettingsState>(
-      builder: (context, state) {
-        return SwitchListTile(
-          title: Text(LocaleKeys.settings_widget_transparentBackground.tr()),
-          secondary: const Icon(Icons.desktop_windows),
-          value: state.desktopWidgetSettings.transparentBackground,
-          onChanged: (value) =>
-              context.read<SettingsCubit>().updateDesktopWidgetSettings(
-                    state.desktopWidgetSettings.copyWith(
-                      transparentBackground: value,
-                    ),
-                  ),
-        );
-      },
-    );
-  }
-}
-
-class _SignOutTile extends StatelessWidget {
-  const _SignOutTile();
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AuthenticationCubit, AuthenticationState>(
-      builder: (context, authState) {
-        if (!authState.signedIn) {
-          return const SizedBox.shrink();
-        }
-
-        return ListTile(
-          title: Text(LocaleKeys.settings_sync_signOut.tr()),
-          leading: const Icon(Icons.logout),
-          onTap: () => _showSignOutDialog(context),
-        );
-      },
-    );
-  }
-
-  /// Shows a dialog to confirm signing out.
-  void _showSignOutDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(LocaleKeys.settings_sync_signOut.tr()),
-          content: Text(LocaleKeys.settings_sync_confirmSignOut.tr()),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text(LocaleKeys.cancel.tr()),
-            ),
-            TextButton(
-              onPressed: () async {
-                final navigator = Navigator.of(context);
-
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (context) => const CircularProgressIndicator(),
-                );
-
-                await authCubit.signOut();
-                navigator.pushReplacementNamed(SignInPage.routeName);
-              },
-              child: Text(LocaleKeys.settings_sync_signOut.tr()),
-            ),
-          ],
-        );
       },
     );
   }
