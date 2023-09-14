0ce28476a8af56adbd9fd5ba4d72356625c78acc
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-09-14 15:05:10 -0400

    chore: upgrade helpers package

--- lib/src/core/helpers/helpers.dart
+++ lib/src/core/helpers/helpers.dart
@@ -1,4 +1,3 @@
 export 'date_time.dart';
 export 'error_handler.dart';
 export 'flatpak.dart';
-export 'media_query.dart';
--- lib/src/core/helpers/media_query.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-extension MediaQueryHelpers on MediaQueryData {
-  /// Matches opposite the `mediumAndUp` breakpoint from the
-  /// flutter_adaptive_scaffold package.
-  bool get isSmallScreen => size.width < 600;
-}
