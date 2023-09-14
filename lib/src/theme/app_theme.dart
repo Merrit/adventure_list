3188ec0fab60ba2fcf239a06eab5906705b08617
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-09-14 15:05:50 -0400

    fix: display emojis in color
    Fallback to Noto Color Emoji is needed to render emojis in color.
    
    See:
    https://github.com/flutter/flutter/issues/119536

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+const _kFallbackFontFamilies = [
+  /// Fallback to Noto Color Emoji is needed to render emojis in color.
+  ///
+  /// See:
+  /// https://github.com/flutter/flutter/issues/119536
+  'Noto Color Emoji',
+];
+
 /// The app's branded color.
 const appColor = Color.fromRGBO(0, 179, 255, 1);
 
@@ -15,6 +23,7 @@ final ThemeData darkTheme = ThemeData(
     ),
   ),
   fontFamily: GoogleFonts.notoSans().fontFamily,
+  fontFamilyFallback: _kFallbackFontFamilies,
 );
 
 /// Light app theme.
@@ -28,4 +37,5 @@ final ThemeData lightTheme = ThemeData(
     ),
   ),
   fontFamily: GoogleFonts.notoSans().fontFamily,
+  fontFamilyFallback: _kFallbackFontFamilies,
 );
