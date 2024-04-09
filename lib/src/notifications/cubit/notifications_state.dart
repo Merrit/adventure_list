c27a04646568dccb17d0759c37c9d3128a3ba164
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-04-09 10:05:04 -0400

    chore: upgrade flutter_local_notifications dependency

--- lib/src/notifications/cubit/notifications_state.dart
+++ lib/src/notifications/cubit/notifications_state.dart
@@ -6,18 +6,24 @@ class NotificationsState with _$NotificationsState {
     required bool enabled,
     NotificationResponse? notificationResponse,
 
+    /// Whether the app has permission to show exact notifications.
+    required bool notificationExactPermissionGranted,
+
+    /// Whether the app has permission to show notifications.
+    required bool notificationPermissionGranted,
+
     /// The number of overdue tasks.
     ///
     /// Tracked so we don't update the icons if the number hasn't changed.
     required int overdueTasksCount,
-    required bool permissionGranted,
   }) = _NotificationsState;
 
   factory NotificationsState.initial() {
     return const NotificationsState(
       enabled: true,
+      notificationExactPermissionGranted: false,
+      notificationPermissionGranted: false,
       overdueTasksCount: 0,
-      permissionGranted: false,
     );
   }
 }
