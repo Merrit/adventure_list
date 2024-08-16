334e70de91575d771e3afa4859ea1f268a2cebd6
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-08-16 12:33:28 -0400

    chore: remove obsolete popupmenu widget
    No longer used, removing it was overlooked.

--- lib/src/core/widgets/widgets.dart
+++ lib/src/core/widgets/widgets.dart
@@ -2,4 +2,3 @@ export 'border.dart';
 export 'custom_reorderable_list_view.dart';
 export 'input_dialog.dart';
 export 'measure_size.dart';
-export 'popup_menu.dart';
--- lib/src/tasks/widgets/tasks_view.dart
+++ lib/src/tasks/widgets/tasks_view.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart' hide PopupMenuButton, PopupMenuItem;
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../generated/locale_keys.g.dart';
