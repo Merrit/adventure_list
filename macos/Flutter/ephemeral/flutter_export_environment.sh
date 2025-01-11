6343980ec131eeb25dd78557d583d78e63a3d924
cs.oi.ne.rr.as.dch@tutanota.de <you@example.com>
2025-01-11 15:29:48 +0100

    build abke windows

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -4,8 +4,13 @@ export "FLUTTER_ROOT=C:\Users\moonmind\Development\flutter\flutter_windows"
 export "FLUTTER_APPLICATION_PATH=C:\Users\moonmind\Development\Android\adventure_list"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
+<<<<<<< Updated upstream
 export "FLUTTER_BUILD_NAME=0.14.3"
 export "FLUTTER_BUILD_NUMBER=29"
+=======
+export "FLUTTER_BUILD_NAME=0.14.2"
+export "FLUTTER_BUILD_NUMBER=28"
+>>>>>>> Stashed changes
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
