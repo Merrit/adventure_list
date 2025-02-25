485836f8429bb4b4dda0dbcecba9598674f68b18
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2025-02-25 13:29:12 -0500

    ci: fix build failure on Windows
    Related to firebase_auth, see:
    https://github.com/firebase/flutterfire/issues/16958

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,8 @@ android {
         applicationId = "codes.merritt.adventurelist"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
